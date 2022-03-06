c44662b19f2dc3c6735eb83e4b2ab2ec60e6d692
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2022-03-06 03:38:22 -0300

    php 8 and laravel octane support

--- src/ProxyScheme/ProxySchemeServiceProvider.php
+++ src/ProxyScheme/ProxySchemeServiceProvider.php
@@ -3,21 +3,44 @@
 namespace RodrigoPedra\ProxyScheme;
 
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Http\Request;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Octane\Events\RequestReceived as OctaneRequestReceived;
 
 class ProxySchemeServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        if (isset($_SERVER['LARAVEL_OCTANE'])) {
+            return;
+        }
+
         $this->app->afterResolving(UrlGenerator::class, function (UrlGenerator $url, Container $app) {
             $request = $app->make(Request::class);
-            $scheme = $request->header('X-Forwarded-Proto');
 
-            if (\in_array($scheme, ['http', 'https'], true)) {
-                $url->forceScheme($scheme);
-            }
+            $this->proxyScheme($request, $url);
         });
+    }
+
+    public function boot(Dispatcher $events)
+    {
+        if (! isset($_SERVER['LARAVEL_OCTANE'])) {
+            return;
+        }
+
+        $events->listen(OctaneRequestReceived::class, function (OctaneRequestReceived $event) {
+            $this->proxyScheme($event->request, $event->app->make(UrlGenerator::class));
+        });
+    }
+
+    public function proxyScheme(Request $request, UrlGenerator $url)
+    {
+        $scheme = $request->header('X-Forwarded-Proto');
+
+        if (\in_array($scheme, ['http', 'https'], true)) {
+            $url->forceScheme($scheme);
+        }
     }
 }
